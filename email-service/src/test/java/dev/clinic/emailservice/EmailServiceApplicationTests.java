176d8f3290246556e75a42effa0960e785a2a455
irvanev <irvvanevv@mail.ru>
2025-02-17 17:33:19 +0300

    Fix

---
+++ email-service/src/test/java/dev/clinic/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.clinic.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
