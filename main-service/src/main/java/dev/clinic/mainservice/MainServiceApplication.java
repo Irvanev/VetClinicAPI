6a2da2eadd417aa749b41f2976d824b241a043cb
AngryL1on <vadim.angryl1on@gmail.com>
2025-03-23 17:38:53 +0300

    Refactor configuration
    Split the configuration application.yml
    Configure Redis
    Configure the necessary dependencies

--- main-service/src/main/java/dev/clinic/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/dev/clinic/mainservice/MainServiceApplication.java
@@ -1,13 +1,10 @@
 package dev.clinic.mainservice;
 
-import dev.clinic.mainservice.models.entities.Role;
-import dev.clinic.mainservice.models.enums.RoleEnum;
-import dev.clinic.mainservice.repositories.RoleRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class MainServiceApplication {
 
